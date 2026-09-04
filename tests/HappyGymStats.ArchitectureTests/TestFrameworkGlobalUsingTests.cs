8b3ab340efbfa6d237ae6d65b58b69994ea1d7ea
geromet <81250489+geromet@users.noreply.github.com>
2026-09-05 01:24:28 +0200

    test: prove architecture xunit global using

---
+++ tests/HappyGymStats.ArchitectureTests/TestFrameworkGlobalUsingTests.cs
@@ -0,0 +1,10 @@
+namespace HappyGymStats.ArchitectureTests;
+
+public sealed class TestFrameworkGlobalUsingTests
+{
+    [Fact]
+    public void Project_owned_xunit_global_using_is_active()
+    {
+        Assert.True(true);
+    }
+}
